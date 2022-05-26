3c646571bda10a0b56cec9696d3070f53585f7e1
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-05-26 17:44:01 +0300

    Exactly end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,35 @@
+import throttle from 'lodash.throttle';
+
+const KEY_LOCALSTORAGE = 'feedback-form-state';
+const formData = {};
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
-const button = document.querySelector('button');
 
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', onTextInputArea);
-textarea.addEventListener('input', onTextInputArea);
-input.addEventListener('input', onTextInputArea);
+form.addEventListener('input', throttle(onTextInputArea, 1000));
 
-const formData = {};
+textarea.addEventListener('input', throttle(onTextInputArea, 1000));
+input.addEventListener('input', throttle(onTextInputArea, 1000));
 
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem('KEY_LOCALSTORAGE');
+  if (!formData[input.name] || !formData[textarea.name]) {
+    alert('Заповніть форму');
+  } else {
+    console.log(formData);
+  }
 }
-
 function onTextInputArea(event) {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem('KEY_LOCALSTORAGE', JSON.stringify(formData));
 }
 
 onExistTextarea();
 function onExistTextarea() {
-  const parsedSettings = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const parsedSettings = JSON.parse(localStorage.getItem('KEY_LOCALSTORAGE'));
   if (parsedSettings) {
     input.value = parsedSettings.email;
     textarea.value = parsedSettings.message;
