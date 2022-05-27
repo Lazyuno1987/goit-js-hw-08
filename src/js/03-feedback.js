3e98e83027c8d042f80585ac690ccbd4103be45c
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-05-27 20:36:51 +0300

    fixed bug

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,12 +14,13 @@ input.addEventListener('input', throttle(onTextInputArea, 1000));
 
 function onFormSubmit(event) {
   event.preventDefault();
-  event.currentTarget.reset();
-  localStorage.removeItem('KEY_LOCALSTORAGE');
+
   if (!formData[input.name] || !formData[textarea.name]) {
     alert('Заповніть форму');
   } else {
     console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem('KEY_LOCALSTORAGE');
   }
 }
 function onTextInputArea(event) {
@@ -31,7 +32,13 @@ onExistTextarea();
 function onExistTextarea() {
   const parsedSettings = JSON.parse(localStorage.getItem('KEY_LOCALSTORAGE'));
   if (parsedSettings) {
-    input.value = parsedSettings.email;
-    textarea.value = parsedSettings.message;
+    if (parsedSettings.email) {
+      input.value = parsedSettings.email;
+      formData[input.name] = parsedSettings.email;
+    }
+    if (parsedSettings.message) {
+      textarea.value = parsedSettings.message;
+      formData[textarea.name] = parsedSettings.message;
+    }
   }
 }
